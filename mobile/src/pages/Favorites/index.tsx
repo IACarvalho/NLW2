0b65d09d742fceacb5df21c8a05fffb5b20c5547
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2020-08-09 20:54:04 -0300

    Dia 04

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+import PageHeader from '../../components/PageHeader';
+
+const Favorites = () => {
+  return (
+    <View style={styles.container}>
+      <PageHeader title='Meus proffys favoritos'/>
+    </View>
+  );
+};
+
+export default Favorites;
